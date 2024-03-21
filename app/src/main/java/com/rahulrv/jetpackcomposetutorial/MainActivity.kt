7d0fa6eec5b5b0b3175da8a94ab287403fdf7b1e
Rahul Ramanujam <rahulramanujam@rahuls-mbp.lan>
2024-03-21 13:55:11 -0700

    initial jetpack compose layout

--- app/src/main/java/com/rahulrv/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/rahulrv/jetpackcomposetutorial/MainActivity.kt
@@ -1,20 +1,101 @@
 package com.rahulrv.jetpackcomposetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rahulrv.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTutorialTheme {
+                Surface (modifier = Modifier.fillMaxSize()) {
+                    MessageCard(
+                        Message(
+                            author = "Android",
+                            body = "Jetpack Compose"
+                        )
+                    )
+                }
+            }
+        }
+    }
 
+    data class Message(
+        val author: String,
+        val body: String
+    )
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        JetpackComposeTutorialTheme {
+            Surface {
+                MessageCard(
+                    Message(
+                        author = "Lexi",
+                        body = "Hey, take a look at Jetpack Compose"
+                    )
+                )
+            }
+        }
     }
 }
